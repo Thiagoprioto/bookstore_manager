a3fad4b7496abf2e616a2ee22794d2a9b764fa38
Thiago <priotothiago@gmail.com>
2026-01-07 21:12:11 -0300

    First commit: project structure created

---
+++ src/main/java/com/thiagoprioto/bookstoremenager/BookstoremenagerApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoprioto.bookstoremenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremenagerApplication.class, args);
+	}
+
+}
