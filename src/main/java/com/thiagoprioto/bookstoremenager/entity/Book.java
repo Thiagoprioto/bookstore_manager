416765a2973a7405aece14cd70445f652636f5bf
Thiago <priotothiago@gmail.com>
2026-01-10 20:18:44 -0300

    Developed project entities

---
+++ src/main/java/com/thiagoprioto/bookstoremenager/entity/Book.java
@@ -0,0 +1,38 @@
+package com.thiagoprioto.bookstoremenager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapter;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "publisher_name",nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @JoinColumn(name= "author_id")
+    private Author author;
+}
